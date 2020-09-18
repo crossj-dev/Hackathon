dfce8093a553b467ba66289947b8542dfe4bf457
crossj-dev <55822856+crossj-dev@users.noreply.github.com>
2020-09-18 18:46:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,11 @@
 # 5 - COMPLETED
 # Maximize data volume transferred speed per second, while minimizing latency
 
+# find latency and bandwidth
+
 import os
-import datetime
+from time import time
+import logging
 from pathlib import Path
 
 # External Packages
@@ -30,6 +33,20 @@ except ImportError:
     from azure.storage.blob import BlobClient, BlobServiceClient, PublicAccess, ContainerClient
     from azure.core.exceptions import AzureError
 
+from tcp_latency import measure_latency
+
+try:
+    from tcp_latency import measure_latency
+except ImportError:
+    os.system("python -m pip install tcp_latency")
+    from tcp_latency import measure_latency
+
+try:
+    import speedtest
+except ImportError:
+    os.system("python -m pip install speedtest-cli")
+    import speedtest
+
 try:
     import PySimpleGUI as sg
 except ImportError:
@@ -39,66 +56,75 @@ except ImportError:
 # Local Imports
 from window import guiWindow
 
-#time_delta = (date_2 - date_1)
-#total_seconds = time_delta.total_seconds()
-#minutes = total_seconds/60
 
-
-def getUploadSpeed(endFileUpload, startFileUpload, total_number):
-    time_delta = (endFileUpload - startFileUpload)
-    total_seconds = time_delta.total_seconds()
+def getUploadSpeed(total_seconds, total_number):
     uploadSpeed = total_number / total_seconds
-    print(
-        f"This {total_number} of files was uploaded in {total_seconds} amount of seconds")
-    print(
+    # log latency metric
+    logging.info(f"Latency: {total_seconds/total_number} ms per file")
+    
+    # log file per s metric
+    logging.info(
+        f"{total_number} files was uploaded in {total_seconds} amount of seconds")
+    logging.info(
         f"Upload Speed: {uploadSpeed} files/second")
 
-def file_upload(folder_path):
+
+def file_upload(folder_path, container_name=""):
 
     conn_str = "DefaultEndpointsProtocol=https;AccountName=rockwellhackathon;AccountKey=eTrtH9I+4HkDId0a/lsLxLd0Nt21izryh7LmqAFttL3EbjkVf1iOUFl3NmVixnMnP0b0fKXRpaGE0E7xiERw8g==;EndpointSuffix=core.windows.net"
     blob_service_client = BlobServiceClient.from_connection_string(
         conn_str=conn_str)
 
     # Create a container called 'folder_name' and Set the permission so the blobs are public.
-    container_name = os.path.basename(folder_path)
+    if container_name == "":
+        container_name = os.path.basename(folder_path)
 
-    # need to do a try catch to handle container already exists error
+    # try catch to handle container already exists error
     try:
         blob_service_client.create_container(
             container_name, public_access=PublicAccess.Container)
-    except Exception as e:
-        print(f'Error in creating container - {e}')
-        return
-    startFileUpload = datetime.datetime.now()
-    # logging.info
-    print(startFileUpload)
+    except Exception:
+        pass
+
     total_number = 0
+    total_upload_time = 0
     for filename in os.listdir(folder_path):
         path = Path(folder_path) / filename
 
         # checks file extension to only upload .csv type
         if path.suffix == '.csv':
             # Upload the created file, use local_file_name for the blob name
-            # try catch to handle container already exists error
+            # try catch to handle file already exists error
             total_number += 1
+            start_time = time()
             try:
                 blob_client = blob_service_client.get_blob_client(
                     container=container_name, blob=filename)
                 with open(path, "rb") as data:
                     blob_client.upload_blob(data)
-            except Exception as e:
-                print(f'Error in creating blobs - {e}')
-                return
-    endFileUpload = datetime.datetime.now()
-    print(endFileUpload)
-    # test this to see if it ouputs correct metrics
+                total_upload_time += (time() - start_time)
+            except Exception:
+                logging.warning(f'Skipping file {filename} due to duplicate')
+
+
+    # log bandwidth metric
+    #s = speedtest.Speedtest()
+    #logging.info(f"Bandwidth Upload Speed: {s.upload()/1000000} Mbps")
+
     # last upload speed 11 files/second
-    getUploadSpeed(endFileUpload, startFileUpload, total_number)
+    getUploadSpeed(total_upload_time, total_number)
 
 
 def main():
     # GUI to select folder to upload to the cloud
     folder_path = ""
+    container_name = ""
+    log = Path('logging.log')
+    if log.exists():
+        os.remove(log)
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info('NEW RUN OF FILE')
+
     try:
         window = guiWindow()
         while True:
@@ -109,15 +135,13 @@ def main():
                 break
             if event == "Continue":
                 folder_path = values['chosen_folder']
+                container_name = values['container_name_user']
                 break
+
+        file_upload(folder_path, container_name.lower())
     except:
         folder_path = ""
-
-    file_upload(folder_path)
 
 
 if __name__ == "__main__":
     main()
-    #loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
-    # loop.close()
